4a5107428a9b638c31ca3d8c0468c9528ecfe5e6
tris84 <jollytris@gmail.com>
2020-03-13 23:18:18 +0900

    edit webview

--- app/src/main/java/com/tr1984/postcodesample/MainActivity.kt
+++ app/src/main/java/com/tr1984/postcodesample/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.tr1984.postcodesample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1000 -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.run {
+                        val address = getStringExtra("address") ?: ""
+                        val extraAddress = getStringExtra("extraAddress") ?: ""
+                        val postCode = getStringExtra("postCode") ?: ""
+                        result.text = "$address $extraAddress $postCode"
+                    }
+                }
+            }
+        }
+    }
+
+    fun goToPostCodeWeb(v: View) {
+        startActivityForResult(Intent(this@MainActivity, PostCodeActivity::class.java), 1000)
     }
 }
--- app/src/main/java/com/tr1984/postcodesample/PostCodeActivity.kt
+++ app/src/main/java/com/tr1984/postcodesample/PostCodeActivity.kt
@@ -1,6 +1,9 @@
 package com.tr1984.postcodesample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_post_code.*
 
@@ -11,10 +14,27 @@ class PostCodeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_post_code)
 
         settingWebView()
-        webview.loadUrl("")
+        webview.loadUrl("https://fir-sample-aaf9e.web.app")
     }
 
     private fun settingWebView() {
+        with(webview.settings) {
+            javaScriptEnabled = true
+            useWideViewPort = true
+        }
+        webview.addJavascriptInterface(PostCodeJavascriptInterface(), "PostCodeJavascriptInterface")
+    }
 
+    inner class PostCodeJavascriptInterface {
+
+        @JavascriptInterface
+        fun onSelectedAddress(addr: String?, extraAddr: String?, postCode: String?) {
+            setResult(Activity.RESULT_OK, Intent().apply {
+                putExtra("address", addr ?: "")
+                putExtra("extraAddress", extraAddr ?: "")
+                putExtra("postCode", postCode ?: "")
+            })
+            finish()
+        }
     }
 }
